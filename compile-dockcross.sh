3db116ae9cb0138c8e9dd289d2c647f4dc8637d6
Joris Gillis <joris.gillis42@gmail.com>
2023-09-26 18:42:15 +0200

    Update compile-dockcross.sh

--- compile-dockcross.sh
+++ compile-dockcross.sh
@@ -20,6 +20,8 @@ for f in *.mo; do
 
     # Unzip the file
     unzip -q $f_fmu -d unzipped
+
+    rm -rf unzipped/binaries/*
     
     # Run the dockcross command
     ./dockcross cmake -Bbuild -DFMI_INTERFACE_HEADER_FILES_DIRECTORY=/work/omc_fmi -DRUNTIME_DEPENDENCIES_LEVEL=none -Hunzipped/sources
