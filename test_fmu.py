965d837150ed3b20b7d0dc4cf05ec4bc51165c27
Joris Gillis <joris.gillis42@gmail.com>
2023-09-27 16:32:17 +0200

    issue #2: path workaround

--- test_fmu.py
+++ test_fmu.py
@@ -2,12 +2,13 @@ from casadi import *
 import zipfile
 import glob
 import shutil
+import os
 
 for f in glob.glob("*.fmu"):
     print(f)
     with zipfile.ZipFile(f, 'r') as zip_ref: zip_ref.extractall(f+".unzipped")
 
-    dae = DaeBuilder('test', f+".unzipped")
+    dae = DaeBuilder('test', os.path.abspath(f+".unzipped"))
     dae.disp(True)
     
     print("provides_directional_derivative", dae.provides_directional_derivative())
